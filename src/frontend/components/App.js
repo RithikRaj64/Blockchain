5d5d315fdbab92912f30d6435307f035cb8e091a
Rithik <rithu0644@gmail.com>
2023-01-13 22:20:40 +0530

    Display account number on React

--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,47 +1,32 @@
-
-import logo from './logo.png';
+import { ethers } from 'ethers';
+import React, { useState } from 'react';
 import './App.css';
- 
+
 function App() {
+
+  const [account, setAccount] = useState(null);
+
+  const web3 = async () => {
+    // Get the accounts given by the Metamask extension into the Browser Window Object
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    setAccount(accounts[0]);
+
+    // Get the Ethereum Provider returned by the Metamask extension 
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    // Get the signer
+
+    // A Signer in ethers is an abstraction of an Ethereum Account
+    // which can be used to sign messages and transactions and 
+    // send signed transactions to the Ethereum Network to execute state changing operations
+
+    const signer = provider.getSigner();
+  }
+
   return (
     <div>
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 ms-3"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Dapp University
-        </a>
-      </nav>
-      <div className="container-fluid mt-5">
-        <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
-            <div className="content mx-auto mt-5">
-              <a
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <img src={logo} className="App-logo" alt="logo"/>
-              </a>
-              <h1 className= "mt-5">Dapp University Starter Kit</h1>
-              <p>
-                Edit <code>src/frontend/components/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="http://www.dappuniversity.com/bootcamp"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-              </a>
-            </div>
-          </main>
-        </div>
-      </div>
+      <button onClick={web3}>Connect</button>
+      <h1>{account}</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.css'
 import App from './frontend/components/App';
 import * as serviceWorker from './serviceWorker';
 
 const rootElement = document.getElementById("root");
-render( <App />, rootElement);
+render(<App />, rootElement);
 
 
 // If you want your app to work offline and load faster, you can change
