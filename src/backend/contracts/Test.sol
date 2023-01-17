41c44eaad12ec7154a4653460209c17468067d10
Rithik <rithu0644@gmail.com>
2023-01-17 15:27:49 +0530

    Track order Contract works

--- src/backend/contracts/Test.sol
+++ src/backend/contracts/Test.sol
@@ -2,9 +2,92 @@
 pragma solidity ^0.8.10;
 
 contract Test {
-    uint256 public test = 100;
+    function add(uint256 _x) external returns (uint256) {
+        return 100 + _x;
+    }
 
-    function add(uint256 _x) external view returns (uint256) {
-        return test + _x;
+    uint64 public orderCount;
+
+    struct Order {
+        uint64 id;
+        address producer;
+        address holder;
+        address consumer;
+        bool delivered;
+        string status;
+        string timestamp;
+        //      timestamp
+    }
+
+    Order[] public Orders;
+
+    mapping(address => bool) isProducer;
+    mapping(address => bool) isMiddleMan;
+    // mapping(uint64 => address) orderToConsumer;
+    mapping(address => uint64[]) consumerToOrders;
+
+    modifier ifProducer() {
+        require(isProducer[msg.sender] == true);
+        _;
+    }
+
+    modifier ifMiddleMan() {
+        require(isMiddleMan[msg.sender] == true);
+        _;
+    }
+
+    function Prod() external view returns (bool) {
+        return isProducer[msg.sender];
+    }
+
+    function Mid() external view returns (bool) {
+        return isMiddleMan[msg.sender];
+    }
+
+    function makeProducer() external {
+        isProducer[msg.sender] = true;
+    }
+
+    function makeMiddleMan() external {
+        isMiddleMan[msg.sender] = true;
+    }
+
+    function placeOrder(address _consumer, string memory _timestamp)
+        external
+        ifProducer
+    {
+        orderCount++;
+        Orders.push(
+            Order(
+                orderCount,
+                msg.sender,
+                msg.sender,
+                _consumer,
+                false,
+                "Order Accepted",
+                _timestamp
+            )
+        );
+        // orderToConsumer[orderCount] = _consumer;
+        consumerToOrders[_consumer].push(orderCount);
+    }
+
+    function passOrder(uint64 _id, string memory _timestamp)
+        external
+        ifMiddleMan
+    {
+        _id--;
+        Orders[_id].holder = msg.sender;
+        Orders[_id].status = "Order Is Being Delivered";
+        Orders[_id].timestamp = _timestamp;
+    }
+
+    function deliveredOrder(uint64 _id, string memory _timestamp) external {
+        _id--;
+        require(msg.sender == Orders[_id].consumer);
+
+        Orders[_id].delivered = true;
+        Orders[_id].status = "Order Has Been Delivered";
+        Orders[_id].timestamp = _timestamp;
     }
 }
--- src/frontend/components/App.js
+++ src/frontend/components/App.js
@@ -1,71 +1,126 @@
-import { ethers } from 'ethers';
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import TestAddress from '../contractsData/Test-address.json';
-import TestAbi from '../contractsData/Test.json';
+import { ethers } from "ethers";
+import React, { useState } from "react";
+import TestAddress from "../contractsData/Test-address.json";
+import TestAbi from "../contractsData/Test.json";
 
 function App() {
-
   const [account, setAccount] = useState(null);
   const [balance, setBalance] = useState("");
   const [conn, setConn] = useState(false);
 
   const [test, setTest] = useState(null);
+  const [order, setOrder] = useState(null);
+  const [pro, setPro] = useState(false);
+  const [mid, setMid] = useState(false);
 
   const web3 = async () => {
     // Get the accounts given by the Metamask extension into the Browser Window Object
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    setAccount(accounts[0]);
+    // const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    // setAccount(accounts[0]);
 
-    // Get the Ethereum Provider returned by the Metamask extension 
+    // Get the Ethereum Provider returned by the Metamask extension
     const provider = new ethers.providers.Web3Provider(window.ethereum);
 
     // Get the signer of the first account
 
     // A Signer in ethers is an abstraction of an Ethereum Account
-    // which can be used to sign messages and transactions and 
+    // which can be used to sign messages and transactions and
     // send signed transactions to the Ethereum Network to execute state changing operations
 
     const signer = provider.getSigner();
 
     // Other way to get the account
-    console.log(await signer.getAddress());
+    const account = await signer.getAddress();
+    setAccount(account);
+    console.log(account);
 
     // Getting balance of the account
-    const bal = ((await signer.getBalance()).toString());
+    const bal = (await signer.getBalance()).toString();
     setBalance(ethers.utils.formatEther(bal));
 
     // Changing the account info
     setConn(true);
 
     loadContracts(signer);
-  }
+  };
 
   // Loading the contracts
   const loadContracts = async (signer) => {
     const Test = new ethers.Contract(TestAddress.address, TestAbi.abi, signer);
     setTest(Test);
+    console.log(test);
+    // await test.makeProducer();
+    // await test.makeMiddleMan();
+  };
 
-    // Calling the functions in the contract
-    console.log((await test.test()).toString());
-  }
+  const time = () => {
+    let date = new Date();
+    let time = `${date.getDate()}/${date.getMonth() + 1
+      }/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    console.log(time);
+    return time;
+  };
 
-  // useEffect(() => {
-  //   setInterval(async () => {
-  //     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const place = async () => {
+    await test.placeOrder("0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC", time());
+  };
 
-  //     if (!(`${accounts[0]}` === `${account}`)) {
-  //       web3();
-  //     }
-  //   }, 1000)
-  // }, []);
+  const pass = async () => {
+    await test.passOrder(1, time());
+  };
+
+  const delivered = async () => {
+    await test.deliveredOrder(1, time());
+  };
+
+  const checkAccountType = asynca() => {
+    setPro(await test.Prod());
+    setMid(await test.Mid());
+  };
+
+  const showOrder = async () => {
+    const orderStr = (await test.Orders(0)).toString();
+    const order = orderStr.split(",");
+    setOrder(order);
+  };
 
   return (
     <div>
-      <button onClick={web3}>{(conn) ? "Change Account" : "Connect"}</button>
+      <button onClick={web3}>{conn ? "Change Account" : "Connect"}</button>
+      <button onClick={checkAccountType}>Account Type</button>
+      <button onClick={showOrder}>Check Order</button>
+
       <h1>Account Address : {account}</h1>
       <h1>Balance : {balance} ETH</h1>
+      <h3>Producer : {pro ? "Yes" : "No"}</h3>
+      <h3>MiddleMan : {mid ? "Yes" : "No"}</h3>
+      <button onClick={place}>Place Order</button>
+      <button onClick={pass}>Pass Order</button>
+      <button onClick={delivered}>Order delivered</button>
+
+      <br />
+      {/* {pro ? (
+      ) : mid ? (
+      ) : (
+      )
+      } */}
+      <div>{order === null ? <></> : <OrderDetails order={order} />}</div>
     </div>
+  );
+}
+
+function OrderDetails(props) {
+  const order = props.order;
+  return (
+    <>
+      <h1>Order Details :</h1>
+      <p>Order Id : {order[0]}</p>
+      <p>Producer Address : {order[1]}</p>
+      <p>Holder Address : {order[2]}</p>
+      <p>Consumer Address : {order[3]}</p>
+      <p>Order Status : {order[5]}</p>
+      <p>Timestamp : {order[6]}</p>
+    </>
   );
 }
 
